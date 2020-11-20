6461e809a2ee22c0d72aff0565b5416653162e61
Hector villegas <hvil23@hotmail.com>
2020-11-19 19:57:55 -0500

    Se agrega autenticacion por google signin

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const client_id = fs.readFileSync(__dirname+'/clientid.key',{encoding:'utf8', flag:'r'});
+
 process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
@@ -9,4 +13,6 @@ process.env.EXPIRED_TOKEN = 60*60*24*30;
 process.env.SEED_TOKEN = process.env.SEED_TOKEN || 'seed-develop';
 
 process.env.DBURL = process.env.NODE_ENV==='dev'?'mongodb://localhost:27017/cafe':process.env.DBURL;
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || client_id;
 
--- server/routes/login.js
+++ server/routes/login.js
@@ -5,12 +5,17 @@ const bcrypt = require('bcrypt');
 
 const jwt = require('jsonwebtoken');
 
+const {OAuth2Client} = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 // Import model DB
 const Usuario = require('../models/usuarios');
 
 // Declare express in app
 const app = express();
 
+// Declare route for login
 app.post('/login', (req, res) => {
 
     let body = req.body;
@@ -55,5 +60,123 @@ app.post('/login', (req, res) => {
 
 });
 
+// Config route google signin
+async function verify( token ) {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.CLIENT_ID
+        });
+        const payload = ticket.getPayload();
+        //const userid = payload['sub'];
+        //console.log(payload.name);
+        //console.log(payload.email);
+
+        return {
+            name: payload.name,
+            email: payload.email,
+            picture: payload.picture,
+            google: true
+        }
+    } catch (err) {
+        return null;
+    }
+}
+//verify().catch(console.error);
+
+// Route google signing
+app.post('/google', async(req, res) => {
+
+    try {
+        let token = req.body.idtoken;
+
+        let googleUser = await verify(token)
+            .catch( err => {
+                return res.status(403).json({
+                    ok: false,
+                    err
+                });
+        });
+    
+        if (googleUser === null || googleUser.email === undefined){
+    
+            res.statusCode = 403;
+            res.setHeader('Content-Type', 'application/json');
+            //res.end('Cannot ' + req.method + ' ' + req.url);
+    
+            return res.status(403).json({
+                ok: false,
+                err: {
+                    message: 'Token google expirado o no valido'
+                }
+            });
+        }
+    
+        Usuario.findOne({ email: googleUser.email },(err,usuarioDB) => {
+            if (err){
+                return res.status(500).json({
+                    ok:false,
+                    err
+                });
+            }
+    
+            if ( usuarioDB ){
+                if ( usuarioDB.google===false ){
+                    return res.status(400).json({
+                        ok: false,
+                        err: {
+                            message: 'Debe usar su autenticacion normal'
+                        }
+                    });
+                }else{
+                    let token = jwt.sign({
+                        usuario: usuarioDB
+                    },process.env.SEED_TOKEN,{ expiresIn:process.env.EXPIRED_TOKEN });
+    
+                    return res.json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token
+                    })
+                }
+            } else {
+                let usuario = new Usuario({
+                    nombre: googleUser.name,
+                    email: googleUser.email,
+                    img: googleUser.picture,
+                    google: googleUser.google,
+                    password: ':)'
+                });
+    
+                usuario.save((err, usuarioDB) => {
+                    if (err){
+                        return res.status(500).json({
+                            ok:false,
+                            err
+                        });
+                    }
+    
+                    let token = jwt.sign({
+                        usuario: usuarioDB
+                    },process.env.SEED_TOKEN,{ expiresIn:process.env.EXPIRED_TOKEN});
+    
+                    return res.json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token
+                    });
+                });
+    
+           }
+        }); 
+    } catch (err) {
+        return res.status(400).json({
+            ok: false,
+            err
+        });
+    }
+    
+
+});
 
 module.exports = app;
